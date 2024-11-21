8f65f6dddeea6590a713b8e24c0e2416bc6ff9d0
Willy Rahman <110042019+willyrahman@users.noreply.github.com>
2024-11-21 12:08:18 +0800

    menambahkan class catatan

---
+++ src/Catatan.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Catatan {
+      private String judul;
+    private String konten;
+    private String tanggal;
+
+    public Catatan(String judul, String konten, String tanggal) {
+        this.judul = judul;
+        this.konten = konten;
+        this.tanggal = tanggal;
+    }
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getKonten() {
+        return konten;
+    }
+
+    public void setKonten(String konten) {
+        this.konten = konten;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    @Override
+    public String toString() {
+        return judul + " (" + tanggal + ")";
+    }
+}
