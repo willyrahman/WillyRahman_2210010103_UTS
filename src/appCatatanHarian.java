c46eb9785ac822a79b02b86c3df1706de6c8d538
Willy Rahman <110042019+willyrahman@users.noreply.github.com>
2024-11-22 10:38:30 +0800

    rombak kode

--- src/appCatatanHarian.java
+++ src/appCatatanHarian.java
@@ -23,13 +23,15 @@ import javax.swing.text.Document;
  */
 public class appCatatanHarian extends javax.swing.JFrame {
     private ArrayList<Catatan> daftarCatatan = new ArrayList<>();
-    private DefaultListModel<Catatan> listModel = new DefaultListModel<>();
+    private DefaultListModel<Catatan> listModel ;
     /**
      * Creates new form appCatatanHarian
      */
     public appCatatanHarian() {
+  
         initComponents();
-        listCatatan.setModel(listModel);
+        listModel = new DefaultListModel<>(); // Initialize the list model
+        listCatatan.setModel(listModel); 
         resetField();
     }
 
@@ -50,8 +52,6 @@ public class appCatatanHarian extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         listCatatan = new javax.swing.JList<>();
-        jCalendar1 = new com.toedter.calendar.JCalendar();
-        jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -60,6 +60,8 @@ public class appCatatanHarian extends javax.swing.JFrame {
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        tanggalText = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -77,14 +79,7 @@ public class appCatatanHarian extends javax.swing.JFrame {
 
         jLabel2.setText("Tulis Catatan :");
 
-        listCatatan.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         jScrollPane2.setViewportView(listCatatan);
-
-        jLabel3.setText("Isi Tanggal Hari ini :");
 
         jButton1.setText("tambah");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -142,6 +137,14 @@ public class appCatatanHarian extends javax.swing.JFrame {
             }
         });
 
+        jLabel3.setText("tulis tanggal  hari ini :");
+
+        tanggalText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tanggalTextActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -152,39 +155,39 @@ public class appCatatanHarian extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel3))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jScrollPane1)
-                                    .addComponent(JudulText))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(41, 41, 41))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(jLabel1))
+                        .addGap(48, 48, 48)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane1)
+                            .addComponent(JudulText))
+                        .addGap(32, 32, 32)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jButton1)
+                                .addComponent(jLabel3)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton4)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton5))
+                                .addComponent(tanggalText, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(147, 147, 147)
-                                .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton6)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton7)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jButton1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButton2)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButton3)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButton4)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButton5))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(147, 147, 147)
+                                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton7)))
                         .addGap(0, 17, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
@@ -204,14 +207,11 @@ public class appCatatanHarian extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(96, 96, 96)
                                 .addComponent(jLabel2)))))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(jLabel3)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(tanggalText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 284, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2)
@@ -221,8 +221,7 @@ public class appCatatanHarian extends javax.swing.JFrame {
                     .addComponent(jButton6)
                     .addComponent(jButton7))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton8)
-                .addGap(21, 21, 21))
+                .addComponent(jButton8))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -231,7 +230,7 @@ public class appCatatanHarian extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 4, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -286,46 +285,49 @@ public class appCatatanHarian extends javax.swing.JFrame {
         // TODO add your handling code here:
         System.exit(0);
     }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void tanggalTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tanggalTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tanggalTextActionPerformed
     private void tambahCatatan() {
-        String judul = JudulText.getText();
-        String konten = areaKonten.getText();
-        Date selectedDate = jCalendar1.getDate();
+                String judul = JudulText.getText();
+            String konten = areaKonten.getText();
+            String tanggal = tanggalText.getText();
 
-        if (!isToday(selectedDate)) {
-            JOptionPane.showMessageDialog(this, "Tanggal yang dipilih harus hari ini!");
-            return;
-        }
-
-        if (judul.isEmpty() || konten.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Judul dan konten harus diisi!");
-            return;
-        }
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        String tanggal = sdf.format(selectedDate);
-        Catatan baru = new Catatan(judul, konten, tanggal);
-        listModel.addElement(baru);
-        daftarCatatan.add(baru);
-        resetField();
-    }
-  private void ubahCatatan() {
-        int index = listCatatan.getSelectedIndex();
-        if (index != -1) {
-            Catatan catatan = daftarCatatan.get(index);
-            Date selectedDate = jCalendar1.getDate();
-
-            if (!isToday(selectedDate)) {
-                JOptionPane.showMessageDialog(this, "Tanggal yang dipilih harus hari ini!");
+            if (judul.isEmpty() || konten.isEmpty() || tanggal.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Judul, konten, dan tanggal harus diisi!");
                 return;
             }
 
-            catatan.setJudul(JudulText.getText());
-            catatan.setKonten(areaKonten.getText());
-            catatan.setTanggal(new SimpleDateFormat("dd-MM-yyyy").format(selectedDate));
-            listModel.set(index, catatan);
-            JOptionPane.showMessageDialog(this, "Catatan berhasil diubah!");
-        } else {
-            JOptionPane.showMessageDialog(this, "Pilih catatan yang ingin diubah!");
-        }
+            if (!isValidDate(tanggal)) {
+                JOptionPane.showMessageDialog(this, "Format tanggal salah. Gunakan format dd-MM-yyyy.");
+                return;
+    }
+
+                Catatan baru = new Catatan(judul, konten, tanggal);
+                listModel.addElement(baru);
+                daftarCatatan.add(baru);
+                resetField();
+    }
+  private void ubahCatatan() {
+                int index = listCatatan.getSelectedIndex();
+           if (index != -1) {
+               Catatan catatan = daftarCatatan.get(index);
+               String tanggal = tanggalText.getText();
+
+               if (!isValidDate(tanggal)) {
+                   JOptionPane.showMessageDialog(this, "Format tanggal salah. Gunakan format dd-MM-yyyy.");
+                   return;
+               }
+
+               catatan.setJudul(JudulText.getText());
+               catatan.setKonten(areaKonten.getText());
+               catatan.setTanggal(tanggal);
+               listModel.set(index, catatan);
+               JOptionPane.showMessageDialog(this, "Catatan berhasil diubah!");
+           } else {
+               JOptionPane.showMessageDialog(this, "Pilih catatan yang ingin diubah!");
+           }
     }
   private void hapusCatatan() {
         int index = listCatatan.getSelectedIndex();
@@ -426,28 +428,34 @@ public class appCatatanHarian extends javax.swing.JFrame {
     }
 }
   private void tampilkanCatatan() {
-        int index = listCatatan.getSelectedIndex();
-        if (index != -1) {
-            Catatan terpilih = daftarCatatan.get(index);
-            JudulText.setText(terpilih.getJudul());
-            areaKonten.setText(terpilih.getKonten());
-            try {
-                Date tanggal = new SimpleDateFormat("dd-MM-yyyy").parse(terpilih.getTanggal());
-                jCalendar1.setDate(tanggal);
-            } catch (Exception ignored) {}
-        }
+            int index = listCatatan.getSelectedIndex();
+         if (index != -1) {
+             Catatan terpilih = daftarCatatan.get(index);
+             JudulText.setText(terpilih.getJudul());
+             areaKonten.setText(terpilih.getKonten());
+             tanggalText.setText(terpilih.getTanggal());
+         }
     }
   
   private void resetField() {
-        JudulText.setText("");
-        areaKonten.setText("");
-        jCalendar1.setDate(new Date());
+            JudulText.setText("");
+            areaKonten.setText("");
+            tanggalText.setText("");
     }
     private boolean isToday(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.format(date).equals(sdf.format(new Date()));
     }
-   
+   private boolean isValidDate(String date) {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+    sdf.setLenient(false);
+    try {
+        sdf.parse(date);
+        return true;
+    } catch (Exception e) {
+        return false;
+    }
+}
     /**
      * @param args the command line arguments
      */
@@ -494,7 +502,6 @@ public class appCatatanHarian extends javax.swing.JFrame {
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
-    private com.toedter.calendar.JCalendar jCalendar1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -502,5 +509,6 @@ public class appCatatanHarian extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList<String> listCatatan;
+    private javax.swing.JTextField tanggalText;
     // End of variables declaration//GEN-END:variables
 }
